d598d62379378bf147cca477f6319e08a795e519
Marco Kroonwijk <marco@kroonwijk.nl>
2014-07-02 00:27:48 +0200

    improved time format

--- setup_polymer_dev.bat
+++ setup_polymer_dev.bat
@@ -1,6 +1,6 @@
 ECHO Create datetime subdirectory
 for /f "tokens=2-4 delims=/- " %%a in ('date /t') do (set mydate=%%c%%b%%a)
-for /f "tokens=1-2 delims=/:" %%a in ("%TIME%") do (set mytime=%%a%%b)
+for /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a%%b)
 
 git checkout master
 git reset --hard origin/master
